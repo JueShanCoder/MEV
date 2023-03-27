e4ab78248a7e00bd71e2a4fa87763889d61e5c55
jueshan <jueshancoder@gmail.com>
2023-03-27 22:59:52 +0800

    commit ReentrtAttack some code.

--- ReentryAttack/contracts/Bank.sol
+++ ReentryAttack/contracts/Bank.sol
@@ -1,18 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 contract Bank {
-    mapping(address => uint) balanceOf;
+    mapping (address => uint256) public balanceOf;    // 余额mapping
 
+    // 存入ether，并更新余额
     function deposit() external payable {
         balanceOf[msg.sender] += msg.value;
     }
 
+    // 提取msg.sender的全部ether
     function withdraw() external {
         uint256 balance = balanceOf[msg.sender]; // 获取余额
         require(balance > 0, "Insufficient balance");
         // 转账 ether !!! 可能激活恶意合约的fallback/receive函数，有重入风险！
         (bool success, ) = msg.sender.call{value: balance}("");
-        require(success, "Failed to send Ether");
+        require(success, "Failed to send Etaher");
         // 更新余额
         balanceOf[msg.sender] = 0;
     }
